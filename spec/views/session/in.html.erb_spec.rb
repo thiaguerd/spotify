88c6b5b6124e93b12ddf2d12483dcac1df8881c8
thiaguerd <mail@thiago.pro>
2020-09-08 08:50:54 -0500

    write simple test to view session/in.slim

--- spec/views/session/in.html.erb_spec.rb
+++ spec/views/session/in.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
-
-RSpec.describe 'session/in.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'session/in.slim', type: :view do
+  it 'have link to in' do
+    render
+    url_in = CGI.escapeHTML(Spotify::Api.url_in(CustomRequest.new))
+    expect(rendered).to match(Regexp.escape(url_in))
+  end
 end
