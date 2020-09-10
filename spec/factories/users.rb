ff1add61b02ed21cfa832124891939afea0aa96c
thiaguerd <mail@thiago.pro>
2020-09-10 07:59:46 -0500

    add model user

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    uid { 'MyString' }
+    name { 'MyString' }
+    external_url { 'MyString' }
+    image_url { 'MyString' }
+    email { 'MyString' }
+  end
+end
