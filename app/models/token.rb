30b87d11e5f4698e4679a9f85b2638db04c81a19
thiaguerd <mail@thiago.pro>
2020-09-11 13:06:53 -0500

    add Token build from response api

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,11 @@
 class Token < ApplicationRecord
   has_one :user, dependent: :destroy
+
+  def self.build(options)
+    Token.create({
+                   access_token: options[:access_token],
+                   refresh_token: options[:refresh_token],
+                   expires_at: DateTime.now + options[:expires_in].seconds
+                 })
+  end
 end
