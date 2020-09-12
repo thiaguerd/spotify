b0399567edf8d626509ee3b60d13f926803288ca
thiaguerd <mail@thiago.pro>
2020-09-12 03:36:12 -0500

    update CustomRequest

--- spec/custom_request.rb
+++ spec/custom_request.rb
@@ -2,7 +2,7 @@ class CustomRequest
   attr_accessor :protocol, :host_with_port
 
   def initialize(options = {})
-    @protocol = options[:protocol] || 'http'
+    @protocol = options[:protocol] || 'http://'
     @host_with_port = options[:host_with_port] || 'test.host'
   end
 end
