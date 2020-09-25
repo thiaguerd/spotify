21d7ef2cdf70913cd6b12c1fc76a4012066a9924
thiaguerd <mail@thiago.pro>
2020-09-24 23:05:05 -0500

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
-  factory :user do
-    uid { 'MyString' }
-    name { 'MyString' }
-    external_url { 'MyString' }
-    image_url { 'MyString' }
-    email { 'MyString' }
+  factory :user do |i|
+    i.sequence(:uid) { |x| "MyString_#{x}" }
+    i.sequence(:name) { |x| "MyString_#{x}" }
+    i.sequence(:external_url) { |x| "MyString_#{x}" }
+    i.sequence(:image_url) { |x| "https://www.img_#{x}.jpg" }
+    i.sequence(:email) { |x| "MyString_#{x}" }
+    token { create(:token) }
   end
 end
