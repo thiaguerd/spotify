79ec97b700f20e10d8cb9480afba9ddf16cb1e83
thiaguerd <mail@thiago.pro>
2020-09-24 23:06:19 -0500

    update test session/in.slim

--- spec/views/session/logged/in.erb_spec.rb
+++ spec/views/session/logged/in.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'session/in.slim', type: :view do
   context 'when logged' do
-    before { allow(view).to receive_messages(current_user: true) }
+    before { allow(view).to receive_messages(current_user: create(:user)) }
 
     it 'have link to in' do
       render template: 'layouts/application'
