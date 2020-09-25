9fe482f1000a6dd5ad92292ecadbbfaf635d9ff5
thiaguerd <mail@thiago.pro>
2020-09-24 23:05:15 -0500

    simple tests to token

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'happy way' do
+    token = build(:token, access_token: 'x', refresh_token: 'y', expires_at: DateTime.now)
+    expect(token).to be_valid
+  end
 end
